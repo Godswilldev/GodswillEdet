f7d2cf83272560ce9aecda673f951421b3f039ee
Godswilldev <71720298+Godswilldev@users.noreply.github.com>
2020-10-25 19:52:59 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-     if (this.scrollY > 100) {
-    $(".siden").addClass("sticky");
-} else {
-    $(".siden").removeClass("sticky")
-}  
+        $(window).scroll(function () {
+            if (this.scrollY > 100) {
+                $(".siden").addClass("sticky");
+                $('.goUp').fadeIn()
+    } else {
+        $(".siden").removeClass("sticky")
+        $('.goUp').fadeOut()
+    }  
+        })
+        
+        $('.works').magnificPopup({
+        delegate: "a",
+        type: "image",
+        gallery: {enabled: true}
     })
-    
-    $('.works').magnificPopup({
-    delegate: "a",
-    type: "image",
-    gallery: {enabled: true}
-})
+
+    $('.goUp').click(function () {
+        scroll(0, 0)
+    })
 });
 
 
