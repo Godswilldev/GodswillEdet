81d3844966f7f35ffc2447424d27480da22a60d6
Godswilldev <71720298+Godswilldev@users.noreply.github.com>
2020-11-10 03:29:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 $(document).ready(function () {
         $(window).scroll(function () {
-            if (this.scrollY > 100) {
-                $(".siden").addClass("sticky");
+            if (this.scrollY > 50) {
+                $(".siden").addClass("derk");
+                $(".navbar").addClass("sticky");
+
                 $('.goUp').fadeIn()
-    } else {
-        $(".siden").removeClass("sticky")
-        $('.goUp').fadeOut()
-    }  
+            } else {
+                $(".siden").removeClass("derk")
+                $(".navbar").removeClass("sticky")
+                $('.goUp').fadeOut()
+            }  
+        })
+        $(".menu-toggler").click(function () {
+            $(".menu-toggler").toggleClass("active");
+            $(".navbar-menu").toggleClass("active")
+        })
+
+        $(".navbar-menu a").click(function () {
+            $(".menu-toggler").removeClass("active");
+            $(".navbar-menu").removeClass("active")
         })
         
         $('.works').magnificPopup({
@@ -16,16 +28,16 @@ $(document).ready(function () {
     })
 
     $('.goUp').click(function () {
-        scroll(0, 0)
+        scroll(0, 0);
     })
 });
 
 // jQuery Ripples
 
 $("section, footer").ripples({
-    resolution: 250,
-    dropRadius: 20,
-    perturbance: 0.04,
+    resolution: 150,
+    dropRadius: 15,
+    perturbance: 0.02,
     interactive: true,
 });
 $("section, footer").ripples("drop", 0.5, 0.5, 1, 1)
@@ -34,10 +46,9 @@ $("section, footer").ripples("play")
 
 
 // Dark Mode scripts
-$(".derk").click(function () {
+$(".siden").click(function () {
     $("body").toggleClass("body-dark");
     $(".section-title").toggleClass("section-title-dark")
     $(".den").toggleClass("section-title-dark")
     $(".block").toggleClass("services-dark")
-
-})
+});
