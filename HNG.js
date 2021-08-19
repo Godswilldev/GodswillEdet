9bbc1ef363c1ff9395f53d7199f0f5e9d88bfb03
Godswill Edet <gimmex1@gmail.com>
2021-08-19 01:04:44 +0100

    hng submission

---
+++ HNG.js
@@ -0,0 +1,3 @@
+// HNG STAGE 2-3 task
+const name = "Godswill Edet";
+console.log(`My name is ${name}`);
--- index.js
+++ index.js
@@ -1,45 +1,43 @@
 $(document).ready(function () {
-        $(window).scroll(function () {
-            if (this.scrollY > 50) {
-                $(".siden").addClass("derk");
-                $(".navbar").addClass("sticky");
+  $(window).scroll(function () {
+    if (this.scrollY > 50) {
+      $(".siden").addClass("derk");
+      $(".navbar").addClass("sticky");
 
-                $('.goUp').fadeIn()
-            } else {
-                $(".siden").removeClass("derk")
-                $(".navbar").removeClass("sticky")
-                $('.goUp').fadeOut()
-            }  
-        })
-        $(".menu-toggler").click(function () {
-            $(".menu-toggler").toggleClass("active");
-            $(".navbar-menu").toggleClass("active")
-        })
+      $(".goUp").fadeIn();
+    } else {
+      $(".siden").removeClass("derk");
+      $(".navbar").removeClass("sticky");
+      $(".goUp").fadeOut();
+    }
+  });
+  $(".menu-toggler").click(function () {
+    $(".menu-toggler").toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
 
-        $(".navbar-menu a").click(function () {
-            $(".menu-toggler").removeClass("active");
-            $(".navbar-menu").removeClass("active")
-        })
-        
-        $('.works').magnificPopup({
-        delegate: "a",
-        type: "image",
-        gallery: {enabled: true}
-    })
+  $(".navbar-menu a").click(function () {
+    $(".menu-toggler").removeClass("active");
+    $(".navbar-menu").removeClass("active");
+  });
 
-    $('.goUp').click(function () {
-        scroll(0, 0);
-    })
+  $(".works").magnificPopup({
+    delegate: "a",
+    type: "image",
+    gallery: { enabled: true },
+  });
+
+  $(".goUp").click(function () {
+    scroll(0, 0);
+  });
 });
 
 // jQuery Ripples
 
-
-
 // Dark Mode scripts
-$(".siden").click(function () {
-    $("body").toggleClass("body-dark");
-    $(".section-title").toggleClass("section-title-dark")
-    $(".den").toggleClass("section-title-dark")
-    $(".block").toggleClass("services-dark")
+$(".siden").click(() => {
+  $("body").toggleClass("body-dark");
+  $(".section-title").toggleClass("section-title-dark");
+  $(".den").toggleClass("section-title-dark");
+  $(".block").toggleClass("services-dark");
 });
